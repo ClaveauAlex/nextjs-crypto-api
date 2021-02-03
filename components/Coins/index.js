b52288c3bea1ef4d45fdae088833a451746707d5
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-03 09:46:30 +0100

    Accessing the api

---
+++ components/Coins/index.js
@@ -0,0 +1,11 @@
+import styles from "./Coins.module.css";
+
+const Coins = ({name}) => {
+    return (
+        <div>
+            <h1>{name}</h1>
+        </div>
+    )
+}
+
+export default Coins;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import CoinList from '../components/CoinList'
 import SearchBar from '../components/SearchBar'
 
-export default function Home() {
+export default function Home({filteredCoins}) {
   return (
     <div>
       <Head>
@@ -9,6 +10,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SearchBar type="text" placeholder="Search"/>
+      <CoinList filteredCoins={filteredCoins}/>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins
+    }
+  }
 }
